8f3df25d5219c504d8f9b14ba0289d9647ed69e5
aksh10patil <rajdeeppatil2021@gmail.com>
2025-02-06 17:41:05 +0530

    daily-dsa

--- DEMO/src/InvereseNumber.java
+++ DEMO/src/InvereseNumber.java
@@ -25,34 +25,5 @@ public class InvereseNumber {
         System.out.println(inv);
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-        // int n = scn.nextInt();
-
-        // int inv  = 0;
-        // int op = 1;  //original position
-        // while ( n != 0) {
-        //     int od = n % 10; //original digit
-        //     int id = op;     //inverse digit , original postion
-        //     int ip = od;     //inverse product , original digit 
- 
-        //     // make changes to inv using ip and id
-        //     inv = inv + id * (int)Math.pow(10, ip-1);
-
-        //     n = n / 10;
-        //     op++;
-        // }
-        // System.out.println(inv);   
     }
 }
