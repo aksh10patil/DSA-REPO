acb595b1f34a82eb7306328dc6fe2e80fa2d691a
aksh10patil <rajdeeppatil2021@gmail.com>
2025-02-08 22:46:28 +0530

    dail-dsa

--- DEMO/src/CeilandFloor.java
+++ DEMO/src/CeilandFloor.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class BinarySearchAlgoforarray { 
+public class CeilandFloor { 
     public static void main(String[] args) {
         int[] arr = { 10, 20 , 30 , 40 , 50 , 60 , 70 , 80, 90, 100};
         Scanner scn = new Scanner(System.in);
@@ -8,6 +8,7 @@ public class BinarySearchAlgoforarray {
 
         int l = 0;
         int h = arr.length - 1;
+
 
         while ( l <= h) {
             int m = (l + h) / 2;
